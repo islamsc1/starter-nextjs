b40f0f219a7c3498eb108a68a53247adef0b8961
islamsc1 <43895797+islamsc1@users.noreply.github.com>
2022-07-21 22:12:57 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Zopry{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
